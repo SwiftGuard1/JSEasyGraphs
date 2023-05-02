cda6c3cfe9a5d141f4b126941714878d640cf11d
SwiftGuard1 <79541415+SwiftGuard1@users.noreply.github.com>
2023-05-02 09:44:03 -0400

    Added animation format

--- animation.js
+++ animation.js
@@ -1,3 +1,17 @@
-class AnimationController {
-    
+/**
+ * A format for a keyframe.
+ * 
+ * Location in percentages.
+ * Property you want to tween.
+ * Value of the property.
+ */
+export const KeyframeFormat = {
+    location: "0%",
+    property: "background-color",
+    value: "red"
+}
+export class AnimationController {
+    createAnimation() {
+        
+    }
 }
