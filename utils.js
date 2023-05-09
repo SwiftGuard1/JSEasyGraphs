f4158701cf7f003b033e519e5ec7dae6e4b3bd51
SwiftGuard1 <79541415+SwiftGuard1@users.noreply.github.com>
2023-05-09 15:12:15 +0000

    Added Random Number Generator in utils.js

---
+++ utils.js
@@ -0,0 +1,17 @@
+function rannumgen(minimum, maximum) {
+    return Math.floor(Math.random() * (1 + maximum - minimum)) + minimum;
+}
+
+export class Utilities {
+    RNG(min, max) {
+        return rannumgen(min, max)
+    }
+
+    RandomNumberGenerator(min, max) {
+        return rannumgen(min, max)
+    }
+
+    RandNumGen(min, max) {
+        return rannumgen(min, max)
+    }
+}
